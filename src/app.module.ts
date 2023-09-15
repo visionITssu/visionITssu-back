c79d02f2b7725e1e6c54ed39b2e65236a668dd15
byeoneunyoung <ebyeon147@soongsil.ac.kr>
2023-09-15 21:45:07 +0900

    Create project structure
    create module and initial setting

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CameraModule } from './camera/camera.module';
+import { PhotoUploadModule } from './photo-upload/photo-upload.module';
+import { CalculateModule } from './calculate/calculate.module';
+import { PhotoEditModule } from './photo-edit/photo-edit.module';
+import { VerificationModule } from './verification/verification.module';
+import { PhotoDownloadService } from './photo-download/photo-download.service';
+import { PhotoDownloadController } from './photo-download/photo-download.controller';
+import { PhotoDownloadModule } from './photo-download/photo-download.module';
+
+@Module({
+  imports: [CameraModule, PhotoUploadModule, CalculateModule, PhotoEditModule, VerificationModule, PhotoDownloadModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/photo-upload/photo-upload.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('photo-upload')
+export class PhotoUploadController {}
