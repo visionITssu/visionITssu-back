08213debb6d36d5e538e4441d2fc7defa781ad10
byeoneunyoung <ebyeon147@soongsil.ac.kr>
2023-09-19 16:51:10 +0900

    Add Swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,6 +7,11 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: "Default API for nest js",
+    type: String,
+  })
   getHello(): string {
     return this.appService.getHello();
   }
