4dca928106ba13200f35d4620d12f10c75476afe
byeoneunyoung <ebyeon147@soongsil.ac.kr>
2023-11-30 20:38:42 +0900

    delete photo-download, photo-upload, testfiles

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CameraModule } from './camera/camera.module';
-import { PhotoUploadModule } from './photo-upload/photo-upload.module';
 import { CalculateModule } from './calculate/calculate.module';
 import { PhotoEditModule } from './photo-edit/photo-edit.module';
-import { PhotoDownloadModule } from './photo-download/photo-download.module';
 import { ExampleModule } from './example/example.module';
 import { SocketModule } from './socket/socket.module';
 import { VerificationModule } from './photo-verification/photo-verification.module';
@@ -14,11 +12,9 @@ import { VerificationModule } from './photo-verification/photo-verification.modu
 @Module({
   imports: [
     CameraModule,
-    PhotoUploadModule,
     CalculateModule,
     PhotoEditModule,
     VerificationModule,
-    PhotoDownloadModule,
     ExampleModule,
     SocketModule,
   ],
--- src/photo-download/photo-download.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PhotoDownloadController } from './photo-download.controller';
-import { PhotoDownloadService } from './photo-download.service';
-
-@Module({
-  controllers: [PhotoDownloadController], // Include the controller
-  providers: [PhotoDownloadService], // Include any services
-})
-export class PhotoDownloadModule {}
